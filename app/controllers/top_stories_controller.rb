35a81c5cc05f9e1fbf18201890190c66bc1ece85
Eduardo Matsushita <egomatsushita@gmail.com>
2018-04-28 16:59:49 -0400

    Generate TopStories controller

---
+++ app/controllers/top_stories_controller.rb
@@ -0,0 +1,2 @@
+class TopStoriesController < ApplicationController
+end
---
+++ test/controllers/top_stories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TopStoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
