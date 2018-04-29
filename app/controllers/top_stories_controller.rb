7852e42f157806b923e92ac6eb819fba0870be8d
Eduardo Matsushita <egomatsushita@gmail.com>
2018-04-29 10:28:02 -0400

    Add first 30 top stories

--- app/controllers/top_stories_controller.rb
+++ app/controllers/top_stories_controller.rb
@@ -1,2 +1,53 @@
+require "net/http"
+require "json"
+
 class TopStoriesController < ApplicationController
+  def index
+    response = get_data("https://hacker-news.firebaseio.com/v0/topstories.json")
+    top_stories = JSON.parse(response) 
+    index_start = 0;
+    index_end = 30;
+    top_stories = top_stories[index_start, index_end] 
+    @stories = []
+    
+    top_stories.each do |story|
+      response = get_data("https://hacker-news.firebaseio.com/v0/item/#{story}.json")
+      response = JSON.parse(response)
+      append_attributes = { 
+        "kids_length" => sum_kids(response["kids"]),
+        "get_time_string" => get_time_string(response["time"])
+      }
+      response.merge!(append_attributes)
+      @stories.push(response)  
+    end
+  end
+  
+  private
+  
+  def get_data(url)
+    uri = URI(url)
+    Net::HTTP.get(uri)
+  end
+
+  def get_time_string(time)
+    now = Time.now.to_i
+    dif = now - time
+    result = nil
+
+    if dif > 86400
+      result = dif < 172800 ? (dif / 86400).to_s + " day ago" : (dif / 86400).to_s + " days ago"
+    elsif dif > 3600
+      result = dif < 7200 ? (dif / 3600).to_s + " hour ago" : (dif / 3600).to_s + " hours ago"
+    else
+      result = dif < 120 ? (dif / 60).to_s + " minute ago" : (dif / 60).to_s + " minutes ago"
+    end
+
+    result
+  end
+
+  def sum_kids(kids)
+    unless kids.nil?
+      kids.count
+    end
+  end
 end
